eeae9af8a6cedf9e41d14c50a42b8eee39e1dd74
Marcos Henrique <marcoshsq@gmail.com>
2022-07-19 16:13:13 -0300

    Update My_notes.sql

--- Course-01-SQL_for_Data_Science/Week_01/My_notes.sql
+++ Course-01-SQL_for_Data_Science/Week_01/My_notes.sql
@@ -6,11 +6,49 @@ Multi-line
 Comment
 */
 
-/* ********************** */
+/* **************************************************************************************** */
 
+
+/*What's SQL?
+
+Structured Query Language (SQL) is a standart computer language for relational database management
+and data manipulation.
+
+Used to query, insert, update and modify data.
+
+SQL ia a non-procedural language (Cannot write complete applications)
+
+SQL is used to Read/retrieve, write and update data.
+
+*/
+
+/* DBA vs Data Scientist
+
+DBA: 
+
+- Manages/governs the DB;
+- Given permissions to users;
+- Determines acess to data;
+- Manages and create Tables;
+
+DS: 
+
+- End user;
+- Uses SQL to query and retrieve data.
+
+*/
+
+/* How Data Scientists uses SQL?
+
+- Retrieve data;;
+- Create their own Tables and environments;
+- Write complex querys for analysis.
+
+*/
 
 /*
 
 
 
 */
+
