bbbc5f684ca9bd2f8e213448bdd6e5b8513ad855
Marcos Henrique <marcoshsq@gmail.com>
2022-07-19 18:32:39 -0300

    Update 03-Creating_Tables.sql

--- Course-01-SQL_for_Data_Science/Week_01/03-Creating_Tables.sql
+++ Course-01-SQL_for_Data_Science/Week_01/03-Creating_Tables.sql
@@ -5,7 +5,7 @@ Create dashboards;
 Visualize data with other tools
 Extract data from other sources
 
-Exmple:*/
+Example:*/
 
 CREATE TABLE Shoes
   (
@@ -15,4 +15,34 @@ CREATE TABLE Shoes
   Color char(250)  Not Null,
   Price decimal(8,2)  Not Null,
   Desc varchar(750)  Null,
+  );
+
+/*Nulls and Primary Keys
+
+- Every column is either null or not null;
+- An error will be returned if one tries to submit a column with no value;
+- Null values != empty strings
+- Primary keys can't be nulls, they must have a value!
+
+*/
+
+/*To insert data into the table*/
+
+INSERT INTO Shoes
+  (
+    Id,
+  Brand,
+  Type,
+  Color,
+  Price,
+  Desc
   )
+VALUES 
+  (
+    "1522",
+    "Gucci",
+    "Slippers",
+    "Pink",
+    "6950,00",
+    "Null"
+  );
