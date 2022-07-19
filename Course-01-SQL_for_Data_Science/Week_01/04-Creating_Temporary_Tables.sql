d710e398b30317f77ef49eb8dc8776f6666327ef
Marcos Henrique <marcoshsq@gmail.com>
2022-07-19 18:52:44 -0300

    Update 04-Creating_Temporary_Tables.sql

--- Course-01-SQL_for_Data_Science/Week_01/04-Creating_Temporary_Tables.sql
+++ Course-01-SQL_for_Data_Science/Week_01/04-Creating_Temporary_Tables.sql
@@ -1,1 +1,12 @@
+/*Why to use a Temporary Table?
 
+- Temporary tables are deleted when the current session is finished;
+- But they are faster than creating regular tables!
+- And useful for complex queries using subsets and joins. */
+
+CREATE TEMPORARY TABLE Sandals As 
+  (
+    SELECT *
+    FROM Shoes
+    WHERE shoe_type = 'sandals'
+  )
