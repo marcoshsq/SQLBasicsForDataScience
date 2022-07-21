8727cb8c9f72c7ab547b72399b9e6ed443419afa
Marcos Henrique <marcoshsq@gmail.com>
2022-07-21 19:08:02 -0300

    Update Day-01-SelectAndDistinct.sql

--- 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-01-SelectAndDistinct.sql
+++ 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-01-SelectAndDistinct.sql
@@ -1,1 +1,13 @@
-Create a new text file
+-- Source: https://www.w3schools.com/sql/sql_select.asp
+
+/* The SQL SELECT Statement
+
+The SELECT statement is used to select data from a database. The data returned is stored in a result table, called the result-set.
+
+SELECT Syntax */
+
+SELECT column1, column2, ...
+FROM table_name;
+
+/*Here, column1, column2, ... are the field names of the table you want to select data from. 
+If you want to select all the fields available in the table, use the following syntax:*/
