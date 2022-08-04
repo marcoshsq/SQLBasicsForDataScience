dfe86e99c64ea2db54c22bc8d83c605ea8070b5d
Marcos Henrique <marcoshsq@gmail.com>
2022-08-03 22:30:29 -0300

    Update Day-02-WhereAndLogicalOperators.sql

--- 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-02-WhereAndLogicalOperators.sql
+++ 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-02-WhereAndLogicalOperators.sql
@@ -14,8 +14,17 @@ WHERE condition;
 
 -- Example:
 
-SELECT * FROM Customers
+SELECT * 
+FROM Customers
 WHERE Country='Mexico';
+
+--or
+
+SELECT * 
+FROM Customers
+WHERE Country='Germany';
+
+--**********************************************************************************************************************
 
 /*Text Fields vs. Numeric Fields
 
@@ -23,10 +32,80 @@ SQL requires single quotes around text values (most database systems will also a
 
 However, numeric fields should not be enclosed in quotes, example: */
 
-SELECT * FROM Customers
+SELECT * 
+FROM Customers
 WHERE CustomerID=1;
+
+SELECT * 
+FROM Customers
+WHERE PostalCode=70563;
+
+--**********************************************************************************************************************
 
 /*Operators in The WHERE Clause
 
-The following operators can be used in the WHERE clause: */
+The following operators can be used in the WHERE clause: 
 
+= |	Equal	
+> |	Greater than	
+< |	Less than	
+>= | Greater than or equal	
+<= | Less than or equal	
+<> or !=| Not equal	
+BETWEEN |	Between a certain range	
+LIKE |	Search for a pattern	
+IN |	To specify multiple possible values for a column
+
+Examples:*/
+
+SELECT * 
+FROM Products
+WHERE Price = 18;
+
+--***
+
+SELECT * 
+FROM Products
+WHERE Price > 30;
+
+--***
+
+SELECT * 
+FROM Products
+WHERE Price < 30;
+
+--***
+
+SELECT * 
+FROM Products
+WHERE Price >= 30;
+
+--***
+
+SELECT * 
+FROM Products
+WHERE Price <= 30;
+
+--***
+
+SELECT * 
+FROM Products
+WHERE Price <> 30;
+
+--***
+
+SELECT * 
+FROM Products
+WHERE Price BETWEEN 50 AND 60;
+
+--***
+
+SELECT * 
+FROM Customers
+WHERE City LIKE 's%';
+
+--**
+
+SELECT * 
+FROM Customers
+WHERE City IN ('Paris','London');
