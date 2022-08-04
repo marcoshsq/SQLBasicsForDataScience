eae35b246c63aa2d237189ff60aca2e51a5c4c77
Marcos Henrique <marcoshsq@gmail.com>
2022-08-03 22:55:35 -0300

    Update and rename My_notes.sql to WHERE-Clause.sql

---
+++ Course-01-SQL_for_Data_Science/Week_02/WHERE-Clause.sql
@@ -0,0 +1,10 @@
+/*The WHERE clause
+
+Used to filter the query, optimizing performance and results
+
+Example: */
+
+SELECT ProdctName, ProductID, UnitPrice
+FROM Products
+WHERE ProductName = "Tofu"
+
