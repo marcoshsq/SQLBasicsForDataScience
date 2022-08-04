19b33649c2801b91827b0e66a9d0eefa8ff77dd3
Marcos Henrique <marcoshsq@gmail.com>
2022-08-03 22:41:11 -0300

    Update Day-02-WhereAndLogicalOperators.sql

--- 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-02-WhereAndLogicalOperators.sql
+++ 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-02-WhereAndLogicalOperators.sql
@@ -109,3 +109,64 @@ WHERE City LIKE 's%';
 SELECT * 
 FROM Customers
 WHERE City IN ('Paris','London');
+
+--**********************************************************************************************************************
+
+/*The SQL AND, OR and NOT Operators
+
+The WHERE clause can be combined with AND, OR, and NOT operators.
+
+The AND and OR operators are used to filter records based on more than one condition:
+
+  The AND operator displays a record if all the conditions separated by AND are TRUE.
+  The OR operator displays a record if any of the conditions separated by OR is TRUE.
+  The NOT operator displays a record if the condition(s) is NOT TRUE.
+
+Examples: */
+
+SELECT * 
+FROM Customers
+WHERE Country='Germany' AND City='Berlin';
+
+-- Only return a statement only from "Customers" where country is "Germany" AND city is "Berlin".
+
+--***
+
+SELECT * 
+FROM Customers
+WHERE City='Berlin' OR City='München';
+
+-- Only return a statement only from "Customers" where city is "Berlin" OR "München"
+
+SELECT * 
+FROM Customers
+WHERE City='Berlin' OR City='München';
+
+-- Only return a statement only from "Customers" where country is "Germany" OR "Spain"
+
+--***
+
+SELECT * 
+FROM Customers
+WHERE NOT Country='Germany';
+
+-- Only return a statement only from "Customers" where country is NOT "Germany"
+
+/*Combining AND, OR and NOT
+
+You can also combine the AND, OR and NOT operators.
+
+The following SQL statement selects all fields from "Customers" where country is "Germany" AND city must be "Berlin" OR "München" 
+(use parenthesis to form complex expressions):*/
+
+SELECT * 
+FROM Customers
+WHERE Country='Germany' 
+  AND (City='Berlin' OR City='München');
+
+--***
+
+SELECT * 
+FROM Customers
+WHERE NOT Country='Germany' 
+  AND NOT Country='USA';
