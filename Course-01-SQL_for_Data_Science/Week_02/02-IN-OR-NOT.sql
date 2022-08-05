fdce74dea470449789d474b19d54181d03cb46ed
Marcos Henrique <marcoshsq@gmail.com>
2022-08-04 21:25:55 -0300

    Update 02-IN-OR-NOT.sql

--- Course-01-SQL_for_Data_Science/Week_02/02-IN-OR-NOT.sql
+++ Course-01-SQL_for_Data_Science/Week_02/02-IN-OR-NOT.sql
@@ -1,1 +1,11 @@
+/*Advanced Filtering: IN, OR, and NOT
 
+These are commands that help optimize queries.
+
+Examples:*/
+
+--IN: The IN is different from the Between because while the BETWEEN Clause gives you a range of values, the IN is specific.
+
+SELECT ProdctID, UnitPrice, SuplierID
+FROM Products
+WHERE SuplierID IN (9, 10, 11);
