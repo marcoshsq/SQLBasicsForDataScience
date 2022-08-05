b619c308669ae8d4f9448652d0131f135cec7702
Marcos Henrique <marcoshsq@gmail.com>
2022-08-04 21:46:27 -0300

    Update 03-Wildcards.sql

--- Course-01-SQL_for_Data_Science/Week_02/03-Wildcards.sql
+++ Course-01-SQL_for_Data_Science/Week_02/03-Wildcards.sql
@@ -1,1 +1,9 @@
+/*SQL Wildcard Characters.
 
+A wildcard character is used to substitute one or more characters in a string.
+
+Wildcard characters are used with the LIKE operator. The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.*/
+
+SELECT * 
+FROM Customers
+WHERE City LIKE 'ber%';
