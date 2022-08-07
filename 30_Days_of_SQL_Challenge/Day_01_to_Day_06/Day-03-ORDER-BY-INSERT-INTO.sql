8b0515c540621829dbcb045771cf80e988d9ea48
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 13:27:39 -0300

    Update Day-03-ORDER-BY-INSERT-INTO.sql

--- 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-03-ORDER-BY-INSERT-INTO.sql
+++ 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-03-ORDER-BY-INSERT-INTO.sql
@@ -57,7 +57,25 @@ ORDER BY Country ASC, CustomerName DESC;
 
 --**********************************************************************************************************************
 
+/* The SQL INSERT INTO Statement
 
+The INSERT INTO statement is used to insert new records in a table.
+
+It is possible to write the INSERT INTO statement in two ways:*/
+
+-- 1. Specify both the column names and the values to be inserted:
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+/* 2. If you are adding values for all the columns of the table, 
+you do not need to specify the column names in the SQL query. 
+However, make sure the order of the values is in the same order 
+as the columns in the table. Here, the INSERT INTO syntax would 
+be as follows:*/
+
+INSERT INTO table_name
+VALUES (value1, value2, value3, ...);
 
 
 
