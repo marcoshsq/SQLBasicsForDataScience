8220cd5667e30e7b70a6403665d6299eb224e8d1
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 20:01:51 -0300

    Update 06-Aggregate-Functions.sql

--- Course 01- SQL for Data Science/Week_02/06-Aggregate-Functions.sql
+++ Course 01- SQL for Data Science/Week_02/06-Aggregate-Functions.sql
@@ -1,1 +1,26 @@
+-- They are predefined functions in sql, which allow us to perform certain analyzes with the data.
 
+-- The MIN() function returns the smallest value of the selected column.
+
+SELECT MIN(Price) AS SmallestPrice
+FROM Products;
+
+-- The MAX() function returns the largest value of the selected column.
+
+SELECT MAX(Price) AS LargestPrice
+FROM Products;
+
+-- The COUNT() function returns the number of rows that matches a specified criterion.
+
+SELECT COUNT(ProductID)
+FROM Products;
+
+-- The AVG() function returns the average value of a numeric column. 
+
+SELECT AVG(Price)
+FROM Products;
+
+-- The SUM() function returns the total sum of a numeric column. 
+
+SELECT SUM(Quantity)
+FROM OrderDetails;
