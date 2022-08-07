7b0f77a94d8099e343f971978d144050c8564787
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 15:54:51 -0300

    Update Day4.sql

--- SQL Challenge/Week 01/Day4.sql
+++ SQL Challenge/Week 01/Day4.sql
@@ -57,3 +57,46 @@ DELETE FROM table_name;
 DELETE FROM Customers;
 
 --**********************************************************************************************************************
+
+-- SQL TOP, LIMIT, FETCH FIRST or ROWNUM Clause
+
+/* The SQL SELECT TOP Clause
+
+The SELECT TOP clause is used to specify the number of records to return.
+
+The SELECT TOP clause is useful on large tables with thousands of records. Returning a large number of records can impact performance.
+
+Not all database systems support the SELECT TOP clause. MySQL supports the LIMIT clause to select a limited number of records, 
+while Oracle uses FETCH FIRST n ROWS ONLY and ROWNUM.*/
+
+-- SQL Server / MS Access Syntax:
+
+SELECT TOP number|percent column_name(s)
+FROM table_name
+WHERE condition;
+
+-- MySQL Syntax:
+
+SELECT column_name(s)
+FROM table_name
+WHERE condition
+LIMIT number;
+
+-- Oracle 12 Syntax:
+
+SELECT column_name(s)
+FROM table_name
+ORDER BY column_name(s)
+FETCH FIRST number ROWS ONLY;
+
+-- Older Oracle Syntax:
+
+SELECT column_name(s)
+FROM table_name
+WHERE ROWNUM <= number;
+
+-- Older Oracle Syntax (with ORDER BY):
+
+SELECT *
+FROM (SELECT column_name(s) FROM table_name ORDER BY column_name(s))
+WHERE ROWNUM <= number;
