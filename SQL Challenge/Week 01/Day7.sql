906ed5731381efe6a294945ad05c2d2e4d6fe6a9
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 18:35:10 -0300

    Update Day7.sql

--- SQL Challenge/Week 01/Day7.sql
+++ SQL Challenge/Week 01/Day7.sql
@@ -150,8 +150,33 @@ FROM Customers;
 
 SELECT CustomerName, CONCAT(Address,', ',PostalCode,', ',City,', ',Country) AS Address
 FROM Customers;
-Note: To get the SQL statement above to work in Oracle use the following:
+
+-- Note: To get the SQL statement above to work in Oracle use the following:
 
 SELECT CustomerName, (Address || ', ' || PostalCode || ' ' || City || ', ' || Country) AS Address
 FROM Customers;
 
+/* Alias for Tables Example
+The following SQL statement selects all the orders from the customer with CustomerID=4 (Around the Horn). We use the "Customers" 
+and "Orders" tables, and give them the table aliases of "c" and "o" respectively (Here we use aliases to make the SQL shorter):*/
+
+SELECT o.OrderID, o.OrderDate, c.CustomerName
+FROM Customers AS c, Orders AS o
+WHERE c.CustomerName='Around the Horn' AND c.CustomerID=o.CustomerID;
+
+-- The following SQL statement is the same as above, but without aliases:
+
+SELECT Orders.OrderID, Orders.OrderDate, Customers.CustomerName
+FROM Customers, Orders
+WHERE Customers.CustomerName='Around the Horn' AND Customers.CustomerID=Orders.CustomerID;
+
+/* 
+
+Aliases can be useful when:
+
+There are more than one table involved in a query
+Functions are used in the query
+Column names are big or not very readable
+Two or more columns are combined together
+
+*/
