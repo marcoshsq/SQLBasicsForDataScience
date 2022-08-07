2bc5d0d502bbf1f233d59cdb28457a16b227d34e
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 16:08:47 -0300

    Update Day5.sql

--- SQL Challenge/Week 01/Day5.sql
+++ SQL Challenge/Week 01/Day5.sql
@@ -29,3 +29,44 @@ FROM Products;
 
 SELECT MIN(Price) AS SmallestPrice
 FROM Products;
+
+--************************************************************************************************************************************
+
+-- COUNT(), AVG() and SUM() Functions
+
+/* The COUNT() function returns the number of rows that matches a specified criterion.
+
+COUNT() Syntax */
+
+SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;
+
+/* The AVG() function returns the average value of a numeric column. 
+
+AVG() Syntax */
+
+SELECT AVG(column_name)
+FROM table_name
+WHERE condition;
+
+/* The SUM() function returns the total sum of a numeric column. 
+
+SUM() Syntax */
+
+SELECT SUM(column_name)
+FROM table_name
+WHERE condition;
+
+-- Examples:
+
+SELECT SUM(Quantity)
+FROM OrderDetails;
+
+SELECT AVG(Price)
+FROM Products;
+
+SELECT COUNT(ProductID)
+FROM Products;
+
+--************************************************************************************************************************************
