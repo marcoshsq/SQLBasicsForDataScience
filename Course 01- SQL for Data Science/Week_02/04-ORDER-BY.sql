f34ecd8e7d2b361b0b37007fd2abf6e218898dca
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 19:07:55 -0300

    Update 04-ORDER-BY.sql

--- Course 01- SQL for Data Science/Week_02/04-ORDER-BY.sql
+++ Course 01- SQL for Data Science/Week_02/04-ORDER-BY.sql
@@ -1,1 +1,17 @@
+/*Often the data in a DB comes in a random order (usually the order in which they were entered), 
+the order by command is used to sort the data and perform a more logical query.*/
 
+-- Syntax
+
+SELECT something
+FROM database
+ORDER BY characteristic
+
+/*Rules:
+
+- Can sort by a column you did not retrieve
+- Must always be the last clause in a select statement
+- Takes the name of one or more columns
+- Separate the items with a comma
+
+*/
