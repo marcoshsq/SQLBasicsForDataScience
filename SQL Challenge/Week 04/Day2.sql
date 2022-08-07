15deffb3fae90de5d42784e3752577f5876b7c81
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 15:39:14 -0300

    Update Day2.sql

--- SQL Challenge/Week 04/Day2.sql
+++ SQL Challenge/Week 04/Day2.sql
@@ -19,3 +19,42 @@ ORDER BY City DESC;
 SELECT * 
 FROM Customers
 ORDER BY Country, City;
+
+--**********************************************************************************************************************
+
+-- Insert
+
+-- 01: Insert a new record in the Customers table.
+
+
+INSERT INTO Customers (
+  CustomerName, 
+  Address, 
+  City, 
+  PostalCode,
+  Country
+  )
+
+VALUES (
+  'Hekkan Burger',
+  'Gateveien 15',
+  'Sandnes',
+  '4306',
+  'Norway'
+  );
+  
+--**********************************************************************************************************************
+
+-- Null
+
+-- 01: Select all records from the Customers where the PostalCode column is empty.
+
+SELECT * 
+FROM Customers
+WHERE PostalCode IS NULL;
+
+-- 02: Select all records from the Customers where the PostalCode column is NOT empty.
+
+SELECT * 
+FROM Customers
+WHERE PostalCode IS NOT NULL;
