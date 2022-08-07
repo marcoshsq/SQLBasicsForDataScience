d0d99905beb538ce9f4948a2aad60770b4d7c172
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 20:13:09 -0300

    Update 07-Grouping-Data.sql

--- Course 01- SQL for Data Science/Week_02/07-Grouping-Data.sql
+++ Course 01- SQL for Data Science/Week_02/07-Grouping-Data.sql
@@ -1,1 +1,15 @@
+/*
 
+The GROUP BY statement groups rows that have the same values into summary rows, 
+like "find the number of customers in each country". 
+The GROUP BY statement is often used with aggregate 
+functions ( COUNT() , MAX() , MIN() , SUM() , AVG() ) 
+to group the result-set by one or more columns.
+
+*/
+
+SELECT column_name(s)
+FROM table_name
+WHERE condition
+GROUP BY column_name(s)
+ORDER BY column_name(s);
