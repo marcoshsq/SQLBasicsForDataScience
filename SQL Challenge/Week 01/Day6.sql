accdee4849e1987f826227fc3f754133f7932d4b
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 16:48:59 -0300

    Update Day6.sql

--- SQL Challenge/Week 01/Day6.sql
+++ SQL Challenge/Week 01/Day6.sql
@@ -111,3 +111,60 @@ _	| Represents a single character |	h_t finds hot, hat, and hit
 -	| Represents any single character within the specified range |	c[a-b]t finds cat and cbt
 
 */
+
+--*****************************************************************************************************************************
+
+-- Examples Using the % Wildcard
+
+-- The following SQL statement selects all customers with a City starting with "ber":
+
+SELECT * 
+FROM Customers
+WHERE City LIKE 'ber%';
+
+-- The following SQL statement selects all customers with a City containing the pattern "es": 
+
+SELECT * 
+FROM Customers
+WHERE City LIKE '%es%';
+
+-- Examples Using the _ Wildcard
+
+-- The following SQL statement selects all customers with a City starting with any character, followed by "ondon":
+
+SELECT * 
+FROM Customers
+WHERE City LIKE '_ondon';
+
+-- The following SQL statement selects all customers with a City starting with "L", followed by any character, followed by "n", 
+-- followed by any character, followed by "on":
+
+SELECT * 
+FROM Customers
+WHERE City LIKE 'L_n_on';
+
+-- Using the [charlist] Wildcard
+
+-- The following SQL statement selects all customers with a City starting with "b", "s", or "p":
+
+SELECT * 
+FROM Customers
+WHERE City LIKE '[bsp]%';
+
+-- The following SQL statement selects all customers with a City starting with "a", "b", or "c":
+
+SELECT * 
+FROM Customers
+WHERE City LIKE '[a-c]%';
+
+-- Using the [!charlist] Wildcard
+
+-- The two following SQL statements select all customers with a City NOT starting with "b", "s", or "p":
+
+SELECT * 
+FROM Customers
+WHERE City LIKE '[!bsp]%';
+
+SELECT * 
+FROM Customers
+WHERE City NOT LIKE '[bsp]%';
