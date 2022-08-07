1ab0df4827d523b383e574d42629f7c192a2e143
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 18:55:34 -0300

    Update Day7.sql

--- SQL Challenge/Week 04/Day7.sql
+++ SQL Challenge/Week 04/Day7.sql
@@ -1,1 +1,24 @@
-Create a new text file
+-- Source: https://www.w3schools.com/sql/exercise.asp?filename=exercise_database1
+
+-- Databases
+
+-- 01: Write the correct SQL statement to create a new database called testDB.
+
+ 
+-- 02: Write the correct SQL statement to delete a database named testDB.
+
+
+-- 03: Write the correct SQL statement to create a new table called Persons.
+
+
+-- 04: Write the correct SQL statement to delete a table called Persons.
+
+
+-- 05: Use the TRUNCATE statement to delete all data inside a table.
+
+
+-- 06: Add a column of type DATE called Birthday.
+
+
+-- 07: Delete the column Birthday from the Persons table.
+
