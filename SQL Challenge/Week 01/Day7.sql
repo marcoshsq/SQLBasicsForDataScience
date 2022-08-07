76c60185d693745e5d5438b2b5720c4a15f7e94d
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 18:17:03 -0300

    Update Day7.sql

--- SQL Challenge/Week 01/Day7.sql
+++ SQL Challenge/Week 01/Day7.sql
@@ -17,3 +17,23 @@ WHERE column_name IN (value1, value2, ...);
 SELECT column_name(s)
 FROM table_name
 WHERE column_name IN (SELECT STATEMENT);
+
+-- Examples:
+
+-- The following SQL statement selects all customers that are located in "Germany", "France" or "UK":
+
+SELECT * 
+FROM Customers
+WHERE Country IN ('Germany', 'France', 'UK');
+
+-- The following SQL statement selects all customers that are NOT located in "Germany", "France" or "UK":
+
+SELECT * 
+FROM Customers
+WHERE Country NOT IN ('Germany', 'France', 'UK');
+
+-- The following SQL statement selects all customers that are from the same countries as the suppliers:
+
+SELECT * 
+FROM Customers
+WHERE Country IN (SELECT Country FROM Suppliers);
