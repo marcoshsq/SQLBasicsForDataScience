b6fc80f39dd8e9c48e5c176699c6eb3ecc695295
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 15:44:28 -0300

    Update Day3.sql

--- SQL Challenge/Week 04/Day3.sql
+++ SQL Challenge/Week 04/Day3.sql
@@ -22,3 +22,13 @@ WHERE CustomerID = 32;
 
 --**********************************************************************************************************************
 
+--Delete
+
+-- 01: Delete all the records from the Customers table where the Country value is 'Norway'.
+
+DELETE FROM Customers
+WHERE Country = 'Norway';
+
+-- 02: Delete all the records from the Customers table.
+
+DELETE FROM Customers;
