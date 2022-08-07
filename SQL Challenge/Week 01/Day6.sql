88d6673f8f57817ad3995ff9613c4919be4cd07a
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 16:42:25 -0300

    Update Day6.sql

--- SQL Challenge/Week 01/Day6.sql
+++ SQL Challenge/Week 01/Day6.sql
@@ -80,3 +80,34 @@ FROM Customers
 WHERE CustomerName NOT LIKE 'a%';
 
 --*****************************************************************************************************************************
+
+/* SQL Wildcard Characters
+
+A wildcard character is used to substitute one or more characters in a string.
+
+Wildcard characters are used with the LIKE operator. The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.
+
+*/
+
+/*
+
+Wildcard Characters in MS Access:
+
+Symbol | Description |	Example
+*	| Represents zero or more characters |	bl* finds bl, black, blue, and blob
+? |	Represents a single character	| h?t finds hot, hat, and hit
+[] |	Represents any single character within the brackets	| h[oa]t finds hot and hat, but not hit
+!	| Represents any character not in the brackets	| h[!oa]t finds hit, but not hot and hat
+-	| Represents any single character within the specified range |	c[a-b]t finds cat and cbt
+#	| Represents any single numeric character |	2#5 finds 205, 215, 225, 235, 245, 255, 265, 275, 285, and 295
+
+Wildcard Characters in SQL Server:
+
+Symbol |	Description |	Example
+%	| Represents zero or more characters |	bl% finds bl, black, blue, and blob
+_	| Represents a single character |	h_t finds hot, hat, and hit
+[]	| Represents any single character within the brackets	| h[oa]t finds hot and hat, but not hit
+^	| Represents any character not in the brackets	h[^oa]t finds | hit, but not hot and hat
+-	| Represents any single character within the specified range |	c[a-b]t finds cat and cbt
+
+*/
