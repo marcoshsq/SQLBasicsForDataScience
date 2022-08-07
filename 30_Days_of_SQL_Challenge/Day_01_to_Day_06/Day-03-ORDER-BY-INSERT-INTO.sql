de3c9caa1e6c9e2ad485f44d3f71c36fc1180876
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 13:19:20 -0300

    Update Day-03-ORDER-BY-INSERT-INTO.sql

--- 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-03-ORDER-BY-INSERT-INTO.sql
+++ 30_Days_of_SQL_Challenge/Day_01_to_Day_06/Day-03-ORDER-BY-INSERT-INTO.sql
@@ -1,1 +1,55 @@
-Create a new text file
+-- Source: https://www.w3schools.com/sql/sql_orderby.asp
+
+/*The SQL ORDER BY Keyword
+
+The ORDER BY keyword is used to sort the result-set in ascending or descending order.
+
+The ORDER BY keyword sorts the records in ascending order by default. To sort the records in descending order, use the DESC keyword.
+
+Syntax: */
+
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+
+/* Example
+
+The following SQL statement selects all customers from the "Customers" table, sorted by the "Country" column:
+
+*/
+
+SELECT * 
+FROM Customers
+ORDER BY Country;
+    
+/* DESC Example
+
+The following SQL statement selects all customers from the "Customers" table, sorted DESCENDING by the "Country" column:
+
+*/
+
+SELECT * 
+FROM Customers
+ORDER BY Country DESC;
+
+/* ORDER BY Several Columns Example
+
+The following SQL statement selects all customers from the "Customers" table, sorted by the 
+"Country" and the "CustomerName" column. This means that it orders by Country, but if some 
+rows have the same Country, it orders them by CustomerName:
+
+*/
+
+SELECT * 
+FROM Customers
+ORDER BY Country, CustomerName;
+
+
+
+
+
+
+
+
+
+
