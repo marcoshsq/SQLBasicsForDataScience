127d3abd089b8590a051c1a39d6cd7c1d5d22c67
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 16:51:37 -0300

    Update Day7.sql

--- SQL Challenge/Week 01/Day7.sql
+++ SQL Challenge/Week 01/Day7.sql
@@ -1,1 +1,19 @@
-Create a new text file
+-- Source: https://www.w3schools.com/sql/sql_in.asp
+
+/* The SQL IN Operator
+
+The IN operator allows you to specify multiple values in a WHERE clause.
+
+The IN operator is a shorthand for multiple OR conditions.
+
+Syntax*/
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
+
+-- or
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (SELECT STATEMENT);
