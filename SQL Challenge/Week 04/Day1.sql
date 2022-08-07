2d524ababb32467e20d69fb3286df78441e22f64
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 15:29:14 -0300

    Update Day1.sql

--- SQL Challenge/Week 04/Day1.sql
+++ SQL Challenge/Week 04/Day1.sql
@@ -1,6 +1,6 @@
 -- Source: https://www.w3schools.com/sql/exercise.asp?filename=exercise_select1
 
--- SQL SELECT
+-- SELECT
 
 -- 01: Insert the missing statement to get all the columns from the Customers table.
 
@@ -16,3 +16,37 @@ FROM Customers;
 
 SELECT DISTINCT Country
 FROM Customers;
+
+--**********************************************************************************************************************
+
+-- WHERE
+
+-- 01: Select all records where the City column has the value "Berlin".
+
+SELECT * 
+FROM Customers
+WHERE City = 'Berlin';
+
+-- 02: Use the NOT keyword to select all records where City is NOT "Berlin".
+
+SELECT * 
+FROM Customers
+WHERE NOT City = 'Berlin';
+
+-- 03: Select all records where the CustomerID column has the value 32.
+
+SELECT * 
+FROM Customers
+WHERE CustomerID = 32;
+
+-- 04: Select all records where the City column has the value 'Berlin' and the PostalCode column has the value 12209.
+
+SELECT * 
+FROM Customers
+WHERE City = 'Berlin' AND PostalCode = 12209;
+
+-- 05: Select all records where the City column has the value 'Berlin' or 'London'.
+
+SELECT * 
+FROM Customers
+WHERE City = 'Berlin' OR City = 'London';
