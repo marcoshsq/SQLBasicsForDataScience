56e0694992c49c6b4cfa3bfbc8c38849f9d872cd
Marcos Henrique <marcoshsq@gmail.com>
2022-08-07 14:18:55 -0300

    Update Day4.sql

--- SQL Challenge/Week 01/Day4.sql
+++ SQL Challenge/Week 01/Day4.sql
@@ -1,1 +1,32 @@
-Create a new text file
+-- Source: https://www.w3schools.com/sql/sql_update.asp
+
+/* The SQL UPDATE Statement
+
+The UPDATE statement is used to modify the existing records in a table.
+
+Syntax*/
+
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+/* UPDATE Table
+
+The following SQL statement updates the first customer (CustomerID = 1) 
+with a new contact person and a new city.*/
+
+UPDATE Customers
+SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+WHERE CustomerID = 1;
+
+/* UPDATE Multiple Records
+
+It is the WHERE clause that determines how many records will be updated.
+
+The following SQL statement will update the ContactName to "Juan" for all records where country is "Mexico":*/
+
+UPDATE Customers
+SET ContactName='Juan'
+WHERE Country='Mexico';
+
+-- Careful when updating records. If you omit the WHERE clause, ALL records will be updated!
