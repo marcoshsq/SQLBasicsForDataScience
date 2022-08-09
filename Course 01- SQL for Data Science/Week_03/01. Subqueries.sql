6b839a7128304363f1b8071b138842a5b3b50faf
Marcos Henrique <marcoshsq@gmail.com>
2022-08-09 20:14:19 -0300

    Update 01. Subqueries.sql

--- Course 01- SQL for Data Science/Week_03/01. Subqueries.sql
+++ Course 01- SQL for Data Science/Week_03/01. Subqueries.sql
@@ -5,14 +5,15 @@ add a new filtering criteria.*/
 /* Example: Need to know the region each customer is 
 from who has had an order with freight over 100. */
 
-SELECT CustomerID, CompanyName, Region
+SELECT CustomerID
+	,CompanyName
+	,Region
 FROM Customers
-WHERE CustomerID IN 
-  (
-    SELECT CustomerID
-    FROM Orders
-    WHERE Freight > 100
-  ); -- SQL always perform the innermost query first 
+WHERE CustomerID IN (
+		SELECT CustomerID
+		FROM Orders
+		WHERE Freight > 100
+		); -- SQL always perform the innermost query first 
 
 --****************************************************************************************
 
@@ -23,14 +24,16 @@ WHERE CustomerID IN
 - Subquery SELECT can only retrieve a single column. 
 */
 
-SELECT    Customer_name, Customer_contact, Customer_ID
+SELECT Customer_name
+	,Customer_contact
+	,Customer_ID
 FROM Customers
-WHERE Customer_ID IN
-  SELECT Customer_ID
-  FROM Orders
-  WHERE Order_number IN 
-  (
-    SELECT Order_number
-    FROM Order_items
-    WHERE Prodct_name = "ToothBrush"
-    );
+WHERE Customer_ID IN (
+		SELECT Customer_ID
+		FROM Orders
+		WHERE Order_number IN (
+				SELECT Order_number
+				FROM Order_items
+				WHERE Prodct_name = "ToothBrush"
+				)
+		);
