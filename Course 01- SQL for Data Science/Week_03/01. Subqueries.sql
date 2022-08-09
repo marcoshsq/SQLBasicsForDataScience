f55d1dd44d09e9dab820c23f0efac38b7a5923f0
Marcos Henrique <marcoshsq@gmail.com>
2022-08-09 20:03:35 -0300

    Update 01. Subqueries.sql

--- Course 01- SQL for Data Science/Week_03/01. Subqueries.sql
+++ Course 01- SQL for Data Science/Week_03/01. Subqueries.sql
@@ -2,11 +2,8 @@
 They merge Data from multiple sources togheter to 
 add a new filtering criteria.*/
 
--- Example:
-
-/*Need to know the region each 
-customer is from who has had an 
-order with freight over 100. */
+/* Example: Need to know the region each customer is 
+from who has had an order with freight over 100. */
 
 SELECT CustomerID, CompanyName, Region
 FROM Customers
@@ -15,4 +12,4 @@ WHERE CustomerID IN
     SELECT CustomerID
     FROM Orders
     WHERE Freight > 100
-  );
+  ); -- SQL always perform the innermost query first 
