a9f32e80b4d1c818cac2a663031a8f932ed71e62
Marcos Henrique <marcoshsq@gmail.com>
2022-08-11 20:45:19 -0300

    Update and rename Joining_Tables.sql to 02. Joining_Tables.sql

---
+++ Course 01- SQL for Data Science/Week_03/02. Joining_Tables.sql
@@ -0,0 +1,32 @@
+/*
+
+Which of the following are benefits of segmenting data in different tables?
+
+Easier data manipulation:
+
+When we want to update a record or change something,
+when that data isn't touching all different levels, it's a lot easier to
+manipulate within tables.    
+
+--
+
+Efficient storage:
+
+When we split out the information into separate
+tables, it helps us store the data more efficiently, so we're not duplicating
+records.    
+
+--
+
+Logically models a process within a business, so it can
+help understand the way a business works in the real world:
+
+How we break the data down into tables is often
+modeled after a business problem or a business process.    
+
+--
+
+Greater scalability:
+
+Breaking data into tables allows us the opportunity to
+scale the information.    */
