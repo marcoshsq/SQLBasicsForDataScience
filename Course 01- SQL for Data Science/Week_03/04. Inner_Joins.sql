1eaa42cb7a991a54745c6d3f2d6c0d365e23fe3f
Marcos Henrique <marcoshsq@gmail.com>
2022-08-11 21:14:25 -0300

    Update 04. Inner_Joins.sql

--- Course 01- SQL for Data Science/Week_03/04. Inner_Joins.sql
+++ Course 01- SQL for Data Science/Week_03/04. Inner_Joins.sql
@@ -9,4 +9,23 @@ SELECT supplier.CompanyName
 	,ProductName
 	,UnitPrice
 FROM Suppliers
-INNER JOIN Products ON Suppliers.supplierID = Products.supplierID
+INNER JOIN Products ON Suppliers.supplierID = Products.supplierID;
+
+/*Notes:
+
+- Join type is specified (INNER JOIN);
+- Join condition is in the FROM clause, and uses the ON clause;
+- Joining more tables affects overall performance;
+- There's no limit on Joins;
+- To make it easier, list all tables, then define the condition.
+
+*/
+
+SELECT o.OrderID
+	,c.CompanyName
+	,e.LastName
+FROM (
+	(
+		Orders O INEER JOIN Customer c ON o.CustomerID = c.CustomerID
+		) INNER JOIN Employees e ON o.EmployeeID = e.EmployeeID
+	);
