bc513af5b5c433b5cff5c4d1ba94192faef77755
Marcos Henrique <marcoshsq@gmail.com>
2022-08-11 21:03:54 -0300

    Update 04. Inner_Joins.sql

--- Course 01- SQL for Data Science/Week_03/04. Inner_Joins.sql
+++ Course 01- SQL for Data Science/Week_03/04. Inner_Joins.sql
@@ -1,1 +1,12 @@
+/*What is an Inner Join?
 
+The INNER JOIN keyword selects records that 
+have matching values in both tables*/
+
+-- Syntax:
+
+SELECT supplier.CompanyName
+	,ProductName
+	,UnitPrice
+FROM Suppliers
+INNER JOIN Products ON Suppliers.supplierID = Products.supplierID
