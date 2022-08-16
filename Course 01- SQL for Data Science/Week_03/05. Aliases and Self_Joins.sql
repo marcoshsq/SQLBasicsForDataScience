2782ec46578b68fa3ae3f3d6dd6ccb151117e199
Marcos Henrique <marcoshsq@gmail.com>
2022-08-15 21:25:50 -0300

    Update 05. Aliases and Self_Joins.sql

--- Course 01- SQL for Data Science/Week_03/05. Aliases and Self_Joins.sql
+++ Course 01- SQL for Data Science/Week_03/05. Aliases and Self_Joins.sql
@@ -7,6 +7,21 @@ the duration of the query.
 Example:*/
 
 SELECT column_name
-FROM table_name AS alias_name
+FROM table_name AS alias_name;
 
+--***************************************************************************************************
 
+/*Self Joins:
+
+Take the table and join it to itself, treating it like two separate tables.
+
+Example: The following query matches customers that are from the same query.*/
+
+SELECT A.CustomerName AS CustomerName1
+	,B.CustomerName AS CustomerName2
+	,A.City
+FROM Customers A
+	,Customers B
+WHERE A.CustomerID = B.CustomerID
+	AND A.City = B.City
+ORDER BY A.City;
