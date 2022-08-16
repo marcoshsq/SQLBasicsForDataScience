a110e87bb0fee9d2c2ee1bab736afdb7f8f4eaf7
Marcos Henrique <marcoshsq@gmail.com>
2022-08-15 21:16:57 -0300

    Update 05. Aliases and Self_Joins.sql

--- Course 01- SQL for Data Science/Week_03/05. Aliases and Self_Joins.sql
+++ Course 01- SQL for Data Science/Week_03/05. Aliases and Self_Joins.sql
@@ -1,1 +1,12 @@
+/*What is an alias:
+
+SQL aliases gives a table or column a temporary name;
+Making the column more readable, and only existis for
+the duration of the query.
+
+Example:*/
+
+SELECT column_name
+FROM table_name AS alias_name
+
 
