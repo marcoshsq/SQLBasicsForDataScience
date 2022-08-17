bfa4590658498630f8e06b24091d3e5c1c060c7b
Marcos Henrique <marcoshsq@gmail.com>
2022-08-17 16:07:29 -0300

    Update Practice Quiz - Writing Queries.sql

--- Course 01- SQL for Data Science/Week_03/Practice Quiz - Writing Queries.sql
+++ Course 01- SQL for Data Science/Week_03/Practice Quiz - Writing Queries.sql
@@ -1,1 +1,21 @@
+-- All of the questions in this quiz pull from the open source Chinook Database.
+
+-- 01. How many albums does the artist Led Zeppelin have? 
+
+
+
+-- 02. Create a list of album titles and the unit prices for the artist "Audioslave".
+
+
+
+-- 03. Find the first and last name of any customer who does not have an invoice.
+
+
+
+-- 04. Find the total price for each album.
+
+
+
+-- 05. How many records are created when you apply a Cartesian join to the invoice and invoice items table?
+
 
