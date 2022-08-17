6d610a4552c4edaacb89c4fba128fbde5b462a0f
Marcos Henrique <marcoshsq@gmail.com>
2022-08-16 22:21:32 -0300

    Update 06. Advanced Joins.sql

--- Course 01- SQL for Data Science/Week_03/06. Advanced Joins.sql
+++ Course 01- SQL for Data Science/Week_03/06. Advanced Joins.sql
@@ -1,4 +1,6 @@
-/*Left Joins vs Right Joins:
+/*
+
+- Left Joins vs Right Joins:
 
 Left Joins returns all the records from the left table (table 01), and the 
 matched records from the right table (table 02).
@@ -10,4 +12,18 @@ matched records from the left table (table 01).
 
 The result is NULL from the left side if there isn't a match.
 
+- Full Outer Join:
+
+Return all records from both tables when there is a match.
+
 */
+
+-- Left Join Example:
+
+-- This example will select all customers and any order they might have.
+
+SELECT C.CustomerName
+	,O.OrderID
+FROM Customers C
+LEFT JOIN Orders O ON C.CustomerID = O.CustomerID
+ORDER BY C.CustomerName;
