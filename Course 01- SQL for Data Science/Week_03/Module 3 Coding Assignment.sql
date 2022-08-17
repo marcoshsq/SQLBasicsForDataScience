389e41fd62e44d8541512ae7d41167def7e3a802
Marcos Henrique <marcoshsq@gmail.com>
2022-08-17 17:58:16 -0300

    Update Module 3 Coding Assignment.sql

--- Course 01- SQL for Data Science/Week_03/Module 3 Coding Assignment.sql
+++ Course 01- SQL for Data Science/Week_03/Module 3 Coding Assignment.sql
@@ -2,28 +2,30 @@
 
 ------------------------------------------------------------------------------------
 
--- 01.
+-- 01. Using a subquery, find the names of all the tracks for the album "Californication".
 
 ------------------------------------------------------------------------------------
 
--- 02. 
+-- 02. Find the total number of invoices for each customer along with the customer's full name, city and email.
 
 ------------------------------------------------------------------------------------
 
--- 03. 
+-- 03. Retrieve the track name, album, artistID, and trackID for all the albums.
 
 ------------------------------------------------------------------------------------
 
--- 04. 
+-- 04. Retrieve a list with the managers last name, and the last name of the employees who report to him or her.
 
 ------------------------------------------------------------------------------------
 
--- 05.
+-- 05. Find the name and ID of the artists who do not have albums. 
 
 ------------------------------------------------------------------------------------
 
--- 06. 
+-- 06. Use a UNION to create a list of all the employee's and customer's first names and last names ordered by the last name in descending order.
 
 ------------------------------------------------------------------------------------
 
--- 07. 
+-- 07. See if there are any customers who have a different city listed in their billing city versus their customer city.
+
+
