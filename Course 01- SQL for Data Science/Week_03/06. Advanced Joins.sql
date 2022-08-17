c7502b7a7a699c5116c02c51c0770594a9dddacb
Marcos Henrique <marcoshsq@gmail.com>
2022-08-16 22:14:18 -0300

    Update 06. Advanced Joins.sql

--- Course 01- SQL for Data Science/Week_03/06. Advanced Joins.sql
+++ Course 01- SQL for Data Science/Week_03/06. Advanced Joins.sql
@@ -1,1 +1,8 @@
+/*Left Joins
 
+Returns all the records from the left table (table 01), and the 
+matched records from the right table (table 02).
+
+The result is NULL from the right side if there isn't a match.
+
+*/
