bcde7fcc261020d18a1abbb95b3e22b65949cdde
Marcos Henrique <marcoshsq@gmail.com>
2022-08-22 09:21:29 -0300

    Update 03. Case Statements.sql

--- Course 01- SQL for Data Science/Week_04/03. Case Statements.sql
+++ Course 01- SQL for Data Science/Week_04/03. Case Statements.sql
@@ -26,3 +26,29 @@ SELECT EmployeeID
 FROM Employees
 ORDER BY FirstName
 	,LastName;
+
+
+/* Search Case Statement:
+
+CASE boolean_expression
+		WHEN when_expression
+			THEN result_expression [...n]
+		ELSE else_result_expression
+		END;
+
+*/
+
+SELECT TrackID
+	,Name
+	,Bytes
+	,CASE 
+		WHEN Bytes < 300000
+			THEN "Small"
+		WHEN Bytes >= 300001
+			AND Bytes <= 500000
+			THEN "Medium"
+		WHEN Bytes >= 500001
+			THEN "Large"
+		ELSE "Other"
+		END BytesCategory
+FROM Tracks;
