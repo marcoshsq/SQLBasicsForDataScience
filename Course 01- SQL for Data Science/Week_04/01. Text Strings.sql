ec24a39929161079641dc629ef2596c31c22113e
Marcos Henrique <marcoshsq@gmail.com>
2022-08-22 08:32:20 -0300

    Update 01. Text Strings.sql

--- Course 01- SQL for Data Science/Week_04/01. Text Strings.sql
+++ Course 01- SQL for Data Science/Week_04/01. Text Strings.sql
@@ -4,3 +4,12 @@ SELECT CompanyName
 	,ContactName
 	,CompanyName || "(" | ContactName | ")"
 FROM Customers
+
+/* Trimming Strings:
+
+Trims the leading or trailing space from a string.
+TRIM
+RTRIM
+LTRIM */
+
+SELECT TRIM ("           Nice!    ")
