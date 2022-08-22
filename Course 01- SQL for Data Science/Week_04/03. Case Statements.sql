a1a121513950eafd61955a2569d7ec807c92020a
Marcos Henrique <marcoshsq@gmail.com>
2022-08-22 09:14:12 -0300

    Update 03. Case Statements.sql

--- Course 01- SQL for Data Science/Week_04/03. Case Statements.sql
+++ Course 01- SQL for Data Science/Week_04/03. Case Statements.sql
@@ -1,1 +1,28 @@
+/*Case Statements.
 
+Case Statements is like a If-Then-Else of other programming languages.
+Works with SELECT, INSERT, UPDATE and DELETE 
+
+Syntax:
+
+CASE input_expression
+		WHEN when_expression
+			THEN result_expression [...n]
+		ELSE else_result_expression
+		END;
+*/
+
+-- Example:
+
+SELECT EmployeeID
+	,FirstName
+	,LastName
+	,City
+	,CASE City
+		WHEN "Calgary"
+			THEN "Calgary"
+		ELSE "Other"
+		END Calgary
+FROM Employees
+ORDER BY FirstName
+	,LastName;
