316f63bb379f793f031922752a9d88096ac051c0
Marcos Henrique <marcoshsq@gmail.com>
2022-08-22 09:02:40 -0300

    Update 02. Date and Time Strings.sql

--- Course 01- SQL for Data Science/Week_04/02. Date and Time Strings.sql
+++ Course 01- SQL for Data Science/Week_04/02. Date and Time Strings.sql
@@ -5,3 +5,13 @@ SELECT BirthDate
 	,STRFTIME("%m", BirthDate) AS Month
 	,STRFTIME("%d", BirthDate) AS Day
 FROM employees;
+
+-- Current Date:
+
+SELECT DATE ("Now");
+
+-- Day, Month and Year for current date:
+
+SELECT STRFTIME("%Y %m %d", "Now");
+
+SELECT STRFTIME("%H %M %S %s", "Now");
