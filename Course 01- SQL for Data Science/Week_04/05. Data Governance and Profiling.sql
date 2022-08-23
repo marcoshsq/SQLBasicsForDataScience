0a2814f3fb9e9a25812d6f2c8c482c191ef69cba
Marcos Henrique <marcoshsq@gmail.com>
2022-08-23 09:28:52 -0300

    Update 05. Data Governance and Profiling.sql

--- Course 01- SQL for Data Science/Week_04/05. Data Governance and Profiling.sql
+++ Course 01- SQL for Data Science/Week_04/05. Data Governance and Profiling.sql
@@ -1,1 +1,21 @@
+/*Profiling your data is where you're looking at either descriptive statistics or different information on the data. 
+And the reason why this is important is because we've talked about how you need to understand your data first, before you start querying it. 
+So profiling is a great step to begin to understand your data. 
+It's really simple things that you can do to start to profile your data.*/
 
+-- Objectives of Data Profile
+
+/* The goal is to get to know your data by doing things like:
+
+- WHat is the column data type; 
+- Number of distinct values;
+- Number of rows with Null values;
+- Some Descriptive Stats: Average, Maximum and Standart Deviation for Columns;
+*/
+
+-- Governance Best Practices
+
+/*
+- Understand the writeing and reading capabilities;
+- Clean up the environment;
+- Understand the promotion process.*/
