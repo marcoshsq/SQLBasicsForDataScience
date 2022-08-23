03c7c2f68d1c8395ac911a22c57341ddb285e785
Marcos Henrique <marcoshsq@gmail.com>
2022-08-23 09:10:53 -0300

    Update 04. Views.sql

--- Course 01- SQL for Data Science/Week_04/04. Views.sql
+++ Course 01- SQL for Data Science/Week_04/04. Views.sql
@@ -1,1 +1,25 @@
+/*Overview of view u.u
 
+- A view is a stored query;
+- Can add or remove columns without changing the schema;
+- Use it to encapsulate queries;
+- The view will be removed after the database connection has endend 
+
+Syntax example:*/
+
+CREATE [TEMP] VIEW [IF NOT EXISTS] view_name(column_name_list) AS slect_statement
+
+-- Exampla:
+
+CREATE VIEW
+AS
+SELECT r.RegionDescription
+	,t.TerritoryDescription
+	,e.LastName
+	,e.FirstName
+	,e.HireDate
+	,e.ReportsTo
+FROM Region r
+INNER JOIN Territories t ON r.RegionID = t.RegionID
+INNER JOIN EmployeesTerritories et ON t.TerritoryID = et.TerritoryID
+INNER JOIN Employees e ON et.EmployeeID = e.EmployeeID
