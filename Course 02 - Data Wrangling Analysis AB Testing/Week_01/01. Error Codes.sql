2bde1f99d9f9ae1d838024105ce503e8c273411c
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 17:36:22 -0300

    Update 01. Error Codes.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_01/01. Error Codes.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_01/01. Error Codes.sql
@@ -1,3 +1,16 @@
+/* Error Codes
+
+Log onto Mode Analytics and from the home page, create a new report by clicking on the green
+plus sign button in the upper right-hand corner. Enter the starter code when provided for each
+exercise. You may want to create a new tab for each exercise.
+
+Please use the discussion forum for any questions and/or comments you might have. Once you
+have tried the exercises, feel free to watch the solutions video. Good luck with your practice!
+Note: When querying a table, remember to prepend dsv1069, which is the schema, or folder
+that contains the course data.
+
+*/
+
 /* Exercise 1:
 
 Goal: Here we use users table to pull a list of user email addresses. Edit the query to pull email
