94c7b6daf0e54e407bf5811db5ec850ca4136553
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 17:31:40 -0300

    Update and rename 01. Introduction to the Course Dataset.sql to 01. Error Codes.sql

---
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_01/01. Error Codes.sql
@@ -0,0 +1,9 @@
+/* Exercise 1:
+Goal: Here we use users table to pull a list of user email addresses. Edit the query to pull email
+addresses, but only for non-deleted users.
+Starter Code:*/
+
+SELECT *
+FROM dsv1069.users
+
+--************************************************************************************************************
