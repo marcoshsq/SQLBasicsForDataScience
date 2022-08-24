268df899bd77b61d875f83a038761301b5ea0a25
Marcos Henrique <marcoshsq@gmail.com>
2022-08-23 23:28:26 -0300

    Update 06. SQL for Data Science.sql

--- Course 01- SQL for Data Science/Week_04/06. SQL for Data Science.sql
+++ Course 01- SQL for Data Science/Week_04/06. SQL for Data Science.sql
@@ -1,1 +1,15 @@
+/*Data Understanding
 
+- Most important step;
+- Understanding relationships on your data;
+- Different types of values (Nulls, Strings, Dates and times, etc.)
+
+Subject Area Understanding
+
+- It's important understand the business;
+- Where data joins are; 
+- Differentiate Strings from Integers.
+
+Always ask questions about the businees problems you want to solve.
+
+*/
