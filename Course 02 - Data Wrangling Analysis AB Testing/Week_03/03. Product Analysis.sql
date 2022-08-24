156d5dac409a6123838a1b764c9438c38c0030e5
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 19:23:20 -0300

    Create 03. Product Analysis.sql

---
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_03/03. Product Analysis.sql
@@ -0,0 +1,44 @@
+/* Product Analysis
+
+Log onto Mode Analytics and from the home page, create a new report by clicking on the green
+plus sign button in the upper right-hand corner. Enter the starter code where provided for each
+exercise. You may want to create a new tab for each exercise.
+
+Please use the discussion forum for any questions and/or comments you might have. Once you
+have tried the exercises, feel free to watch the solutions video. Good luck with your practice!
+
+Note: When querying a table, remember to prepend dsv1069, which is the schema, or folder
+that contains the course data.
+
+*/
+
+Exercise 0: Count how many users we have
+Starter Code:
+
+Exercise 1: Find out how many users have ever ordered
+Starter Code:
+
+Exercise 2:
+--Goal find how many users have reordered the same item
+Starter Code:
+
+Exercise 3:
+--Do users even order more than once?
+Starter Code:
+
+Exercise 4:
+--Orders per item
+Starter Code:
+
+Exercise 5:
+--Orders per category
+Starter Code:
+
+Exercise 6:
+--Goal: Do user order multiple things from the same category?
+Starter Code:
+
+Exercise 7:
+--Goal: Find the average time between orders
+--Decide if this analysis is necessary
+Starter Code:
