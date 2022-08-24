34b095b19ca2345aa8cf3757db12d91fed9935f6
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 17:34:35 -0300

    Update 01. Error Codes.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_01/01. Error Codes.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_01/01. Error Codes.sql
@@ -1,9 +1,39 @@
 /* Exercise 1:
+
 Goal: Here we use users table to pull a list of user email addresses. Edit the query to pull email
 addresses, but only for non-deleted users.
+
 Starter Code:*/
 
 SELECT *
 FROM dsv1069.users
 
 --************************************************************************************************************
+
+/*Exercise 2:
+
+--Goal: Use the items table to count the number of items for sale in each category
+
+Starter Code: (none)*/
+
+
+
+--************************************************************************************************************
+
+/*Exercise 3:
+
+--Goal: Select all of the columns from the result when you JOIN the users table to the orders
+table
+
+Starter Code: (none)*/
+
+
+
+--************************************************************************************************************
+
+/*Exercise 4:
+
+--Goal: Check out the query below. This is not the right way to count the number of viewed_item
+events. Determine what is wrong and correct the error.
+
+Starter Code:*/
