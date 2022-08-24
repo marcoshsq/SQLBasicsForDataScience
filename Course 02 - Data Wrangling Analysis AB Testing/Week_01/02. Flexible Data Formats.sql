2916a67032f40ea4d42575886bb4a4bcb421b798
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 18:33:46 -0300

    Create 02. Flexible Data Formats.sql

---
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_01/02. Flexible Data Formats.sql
@@ -0,0 +1,35 @@
+/* Flexible Data Formats
+
+Log onto Mode Analytics and from the home page, create a new report by clicking on
+the green plus sign button in the upper right-hand corner. Enter the starter code
+provided for each exercise. You may want to create a new tab for each exercise.
+Please use the discussion forum for any questions and/or comments you might have.
+Once you have tried the exercises, feel free to watch the solutions video. Good luck with
+your practice!*/
+
+-- *************************************************************************************************************************************************
+
+/* Exercise 1:
+
+Goal: Write a query to format the view_item event into a table with the appropriate columns.
+
+Code:*/
+
+-- *************************************************************************************************************************************************
+
+/* Exercise 2:
+
+Goal: Write a query to format the view_item event into a table with the appropriate columns
+(This replicates what we had in the slides, but it is missing a column).
+
+Code:*/
+
+-- *************************************************************************************************************************************************
+
+/* Exercise 3:
+
+Goal: Use the result from the previous exercise, but make sure
+
+Code:*/
+
+-- *************************************************************************************************************************************************
