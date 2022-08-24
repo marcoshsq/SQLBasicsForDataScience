9b28e70d5b3085466c02b0b72468b9407431abe1
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 20:08:13 -0300

    Update 02. Create a New Metric.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_04/02. Create a New Metric.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_04/02. Create a New Metric.sql
@@ -1,1 +1,31 @@
+/* Create a Test Metric
+Log onto Mode Analytics and from the home page, create a new report by clicking on the green
+plus sign button in the upper right-hand corner. Enter the starter code where provided for each
+exercise. You may want to create a new tab for each exercise.
 
+Please use the discussion forum for any questions and/or comments you might have. Once you
+have tried the exercises, feel free to watch the solutions video. Good luck with your practice!
+
+Note: When querying a table, remember to prepend dsv1069, which is the schema, or folder
+that contains the course data.*/
+
+-- ********************************************************************************************************
+
+/* Exercise 1: Using the table from Exercise 4.3 and compute a metric that measures
+
+1) Whether a user created an order after their test assignment
+2) Requirements: Even if a user had zero orders, we should have a row that counts
+3) their number of orders as zero
+4) If the user is not in the experiment they should not be included
+
+Code: */
+
+-- ********************************************************************************************************
+
+/* Exercise 2: Using the table from the previous exercise, add the following metrics
+
+1) the number of orders/invoices
+2) the number of items/line-items ordered
+3) the total revenue from the order after treatment
+
+Code: */
