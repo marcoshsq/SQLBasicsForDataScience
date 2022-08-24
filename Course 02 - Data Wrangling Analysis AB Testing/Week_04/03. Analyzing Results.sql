9cdebb556520f0fd81eb36a7077abbae1dafe83f
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 20:10:14 -0300

    Update 03. Analyzing Results.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_04/03. Analyzing Results.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_04/03. Analyzing Results.sql
@@ -1,1 +1,34 @@
+/* Analyzing Results
 
+Log onto Mode Analytics and from the home page, create a new report by clicking on the green
+plus sign button in the upper right-hand corner. Enter the starter code where provided for each
+exercise. You may want to create a new tab for each exercise.
+
+Please use the discussion forum for any questions and/or comments you might have. Once you
+have tried the exercises, feel free to watch the solutions video. Good luck with your practice!
+
+Note: When querying a table, remember to prepend dsv1069, which is the schema, or folder
+that contains the course data. */
+
+-- ************************************************************************************************
+
+/* Exercise 1: Use the order_binary metric from the previous exercise, count the number of users
+per treatment group for test_id = 7, and count the number of users with orders (for test_id 7) */
+
+-- ************************************************************************************************
+
+/* Exercise 2: Create a new tem view binary metric. Count the number of users per treatment
+group, and count the number of users with views (for test_id 7) */
+
+-- ************************************************************************************************
+
+/* Exercise 3: Alter the result from EX 2, to compute the users who viewed an item WITHIN 30
+days of their treatment event */
+
+-- ************************************************************************************************
+
+/* Exercise 4: Create the metric invoices (this is a mean metric, not a binary metric) and for test_id = 7
+
+- The count of users per treatment group
+- The average value of the metric per treatment group
+- The standard deviation of the metric per treatment group */
