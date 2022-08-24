ecb7a7eecf940394d6cd44e5a2074265d2a63349
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 19:20:45 -0300

    Create 02. Promo Email.sql

---
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_03/02. Promo Email.sql
@@ -0,0 +1,40 @@
+/* Promo Email
+
+Log onto Mode Analytics and from the home page, create a new report by clicking on the green
+plus sign button in the upper right-hand corner. Enter the starter code where provided for each
+exercise. You may want to create a new tab for each exercise.
+
+Please use the discussion forum for any questions and/or comments you might have. Once you
+have tried the exercises, feel free to watch the solutions video. Good luck with your practice!
+
+Note: When querying a table, remember to prepend dsv1069, which is the schema, or folder
+that contains the course data. */
+
+-- ****************************************************************************************************
+
+/* Exercise 1: Create the right subtable for recently viewed events using the view_item_events table.
+
+Code: */ 
+
+-- ****************************************************************************************************
+
+/* Exercise 2: Check your joins. Join your tables together recent_views, users, items
+
+Code: */
+
+-- ****************************************************************************************************
+
+/* Exercise 3: Clean up your columns. The goal of all this is to return all of the information we’ll
+need to send users an email about the item they viewed more recently. 
+
+Clean up this query outline from the outline in EX2 and pull only the columns you need. Make sure they are named
+appropriately so that another human can read and understand their contents.
+
+Code: */
+
+-- ****************************************************************************************************
+
+/* Exercise 4: Consider any edge cases. If we sent an email to everyone in the results of this
+query, what would we want to filter out. Add in any extra filtering that you think would make this
+email better. For example should we include deleted users? Should we send this email to users
+who already ordered the item they viewed most recently?
