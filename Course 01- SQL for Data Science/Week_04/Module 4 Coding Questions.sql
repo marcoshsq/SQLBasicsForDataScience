610ae45f3a52e84aa1a4481b5563c2e508e371f0
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 11:20:44 -0300

    Update Module 4 Coding Questions.sql

--- Course 01- SQL for Data Science/Week_04/Module 4 Coding Questions.sql
+++ Course 01- SQL for Data Science/Week_04/Module 4 Coding Questions.sql
@@ -1,18 +1,26 @@
-/*
-
-
-
+/*Pergunta 1
+All of the questions in this quiz refer to the open source Chinook Database. 
+Please familiarize yourself with the ER diagram in order to familiarize yourself with the 
+table and column names in order to write accurate queries and get the appropriate answers.
 */
 
--- 01:
+/* 01:Pull a list of customer ids with the customer’s full name, and address, along with combining their city and country together. 
+Be sure to make a space in between these two and make it UPPER CASE. (e.g. LOS ANGELES USA)*/
 
--- 02:
+/* 02: Create a new employee user id by combining the first 4 letters of the employee’s first name with the first 2 letters of the employee’s last name. 
+Make the new field lower case and pull each individual step to show your work.*/
 
--- 03: 
+-- 03: Show a list of employees who have worked for the company for 15 or more years using the current date function. Sort by lastname ascending.
 
--- 04: 
+-- 04: Profiling the Customers table, answer the following question. Are there any columns with null values? Indicate any below. Select all that apply.
 
--- 05: 
 
--- 06: 
+
+-- 05: Find the cities with the most customers and rank in descending order.
+
+
+
+/* 06: Create a new customer invoice id by combining a customer’s invoice id 
+with their first and last name while ordering your query in the following order: firstname, 
+lastname, and invoiceID */
 
