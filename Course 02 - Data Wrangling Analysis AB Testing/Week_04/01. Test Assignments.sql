4efd55c1443e996e24438c45e02b249d7a3eac4d
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 19:38:24 -0300

    Update 01. Test Assignments.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_04/01. Test Assignments.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_04/01. Test Assignments.sql
@@ -1,1 +1,34 @@
+/* Test Assignments
 
+Log onto Mode Analytics and from the home page, create a new report by clicking on the green
+plus sign button in the upper right-hand corner. Enter the starter code where provided for each
+exercise. You may want to create a new tab for each exercise.
+
+Please use the discussion forum for any questions and/or comments you might have. Once you
+have tried the exercises, feel free to watch the solutions video. Good luck with your practice!
+
+Note: When querying a table, remember to prepend dsv1069, which is the schema, or folder
+that contains the course data.*/
+
+-- ********************************************************************************************
+
+-- Exercise 1: Figure out how many tests we have running right now
+
+-- ********************************************************************************************
+
+/* Exercise 2: Check for potential problems with test assignments. For example Make sure there
+is no data obviously missing (This is an open ended question)*/
+
+-- ********************************************************************************************
+
+/* Exercise 3: Write a query that returns a table of assignment events.Please include all of the
+relevant parameters as columns (Hint: A previous exercise as a template)*/
+
+-- Code:
+
+-- ********************************************************************************************
+
+/* Exercise 4: Check for potential assignment problems with test_id 5. Specifically, make sure
+users are assigned only one treatment group.*/
+
+-- ********************************************************************************************
