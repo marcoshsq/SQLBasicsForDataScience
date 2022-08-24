2fdb23f18d6318215a6ba6fcd0d88aedd7741306
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 19:17:23 -0300

    Update and rename My_notes.sql to 01. Create a Rollup Table.sql

---
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_03/01. Create a Rollup Table.sql
@@ -0,0 +1,48 @@
+/* Create a Rollup Table
+
+Log onto Mode Analytics and from the home page, create a new report by clicking on the green
+plus sign button in the upper right-hand corner. Enter the starter code where provided for each
+exercise. You may want to create a new tab for each exercise.
+
+Please use the discussion forum for any questions and/or comments you might have. Once you
+have tried the exercises, feel free to watch the solutions video. Good luck with your practice!
+
+Note: When querying a table, remember to prepend dsv1069, which is the schema, or folder
+that contains the course data. */
+
+-- ********************************************************************************************************************
+
+/* Exercise 1: Create a subtable of orders per day. Make sure you decide whether you are
+counting invoices or line items.
+
+Code: */
+
+-- ********************************************************************************************************************
+
+/* Exercise 2: “Check your joins”. We are still trying to count orders per day. In this step join the
+sub table from the previous exercise to the dates rollup table so we can get a row for every
+date. Check that the join works by just running a “select *” query
+
+Code: */
+
+-- ********************************************************************************************************************
+
+/* Exercise 3: “Clean up your Columns” In this step be sure to specify the columns you actually
+want to return, and if necessary do any aggregation needed to get a count of the orders made
+per day.
+
+Code: */
+
+-- ********************************************************************************************************************
+
+/* Exercise 4: Weekly Rollup. Figure out which parts of the JOIN condition need to be edited
+create 7 day rolling orders table.
+
+Code: */
+
+-- ********************************************************************************************************************
+
+/* Exercise 5: Column Cleanup. Finish creating the weekly rolling orders table, by performing
+any aggregation steps and naming your columns appropriately.
+
+Code: */
