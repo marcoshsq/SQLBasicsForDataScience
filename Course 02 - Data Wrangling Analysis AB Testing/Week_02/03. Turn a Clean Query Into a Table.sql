4f03584c64e2028ac027b5495008eb52ff03327e
Marcos Henrique <marcoshsq@gmail.com>
2022-08-25 09:24:05 -0300

    Update 03. Turn a Clean Query Into a Table.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_02/03. Turn a Clean Query Into a Table.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_02/03. Turn a Clean Query Into a Table.sql
@@ -1,5 +1,17 @@
 -- Turn a Clean Query Into a Table 
 
+-- Here we create the table structure:
+CREATE TABLE "view_item_event" (
+	event_id VARCHAR(32) NOT NULL PRIMARY KEY
+	,event_time VARCHAR(26)
+	,user_id INT (10)
+	,platform VARCHAR(10)
+	,item_id INT (10)
+	,referrer VARCHAR(17)
+	);
+
+INSERT INTO "view_item_event"
+
 -- Initial query:
 CREATE TABLE view_item_event_1 AS
 SELECT event_id
@@ -25,12 +37,3 @@ GROUP BY event_id
 ORDER BY event_id;
 
 
--- Here we create the table structure:
-CREATE TABLE "view_item_event" (
-	event_id VARCHAR(32) NOT NULL PRIMARY KEY
-	,event_time VARCHAR(26)
-	,user_id INT (10)
-	,platform VARCHAR(10)
-	,item_id INT (10)
-	,referrer VARCHAR(17)
-	);
