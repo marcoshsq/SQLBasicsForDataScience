4fd61cad01897d04a50832edbbde51c9fdf2fe15
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 22:45:40 -0300

    Update 01. Data Types.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_02/01. Data Types.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_02/01. Data Types.sql
@@ -1,1 +1,27 @@
+-- Data Types:
 
+-- 01. Which of the following is easier to read?
+
+SELECT column_1
+	,column_2
+	,count(*)
+FROM example_table
+WHERE column_3 IS NOT NULL
+GROUP BY column_1
+	,column_2
+  
+-- 02. Suppose in a table you find a column called username, which contains the value kat123. What is the correct data type category for this column?
+
+String
+
+-- 03. Suppose in a table you find a column called price, which contains the value $9.99. What is the best data type category for this column?
+
+Number
+
+-- 04. Suppose in a table you find a column called created_at, which contains the value 2019-01-01. What is the best data type category for this column?
+
+Date
+
+-- 05. Suppose in a table you find a column called price, which contains the value $9.99. Of the following options, which is the best data type for this column?
+
+Float
