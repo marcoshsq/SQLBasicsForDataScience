2679cb11c7c30d23477cc8f744c5e94e82872202
Marcos Henrique <marcoshsq@gmail.com>
2022-08-25 09:13:17 -0300

    Update 03. Turn a Clean Query Into a Table.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_02/03. Turn a Clean Query Into a Table.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_02/03. Turn a Clean Query Into a Table.sql
@@ -1,5 +1,22 @@
-CREATE TABLE view_item_event_1 AS  
-SELECT event_id, event_time, user_id, platform, MAX (CASE WHEN parameter_name = "item_id" THEN paramater_value ELSE NULL END) AS item_id, MAX (CASE WHEN parameter_name = "referrer" THEN paramater_value ELSE NULL END) AS referrer
+CREATE TABLE view_item_event_1 AS
+
+SELECT event_id
+	,event_time
+	,user_id
+	,platform
+	,MAX(CASE 
+			WHEN parameter_name = "item_id"
+				THEN paramater_value
+			ELSE NULL
+			END) AS item_id
+	,MAX(CASE 
+			WHEN parameter_name = "referrer"
+				THEN paramater_value
+			ELSE NULL
+			END) AS referrer
 FROM events
 WHERE event_name = "view_item"
-GROUP BY event_id, event_time, user_id, platform
+GROUP BY event_id
+	,event_time
+	,user_id
+	,platform
