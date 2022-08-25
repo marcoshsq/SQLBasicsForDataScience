ebc9746b59aabf4dd2cb5bf105589f974e0acc65
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 22:53:27 -0300

    Update 02. What is a Dependency?.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_02/02. What is a Dependency?.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_02/02. What is a Dependency?.sql
@@ -1,17 +1,23 @@
 -- What is a Dependency?
 
--- 01. 
+-- 01. ETL stands for:
 
+Extract, Transform and Load
 
+-- 02. If I say that table X is dependent on table Y which table should be generated (or refreshed) first?
 
--- 02. 
+Table Y
 
+-- 03. Based on the material covered in this lesson What is a Dependency? which of the following statements is true?
 
+The view_items table depends on the events table
 
--- 03. 
+-- 04. Based on the code snippet below, which statements are definitely true: 
 
+CREATE TABLE table_x AS
+SELECT date
+       ,COUNT(*)
+FROM table_y
+GROUP BY date;
 
-
--- 04. 
-
-
+Table_x is dependent on table_y
