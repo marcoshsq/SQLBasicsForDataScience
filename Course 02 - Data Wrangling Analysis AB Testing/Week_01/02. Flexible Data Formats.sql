f23a1ffe5d1c482ffa73b85f9f74a19d2d4a2366
Marcos Henrique <marcoshsq@gmail.com>
2022-08-24 21:27:44 -0300

    Update 02. Flexible Data Formats.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_01/02. Flexible Data Formats.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_01/02. Flexible Data Formats.sql
@@ -19,6 +19,25 @@ SELECT event_id
 	,event_time
 	,user_id
 	,platform
+	,parameter_name
+	,parameter_value
+FROM dsv1069.events
+WHERE event_name = 'view_item'
+ORDER BY event_id;
+  
+-- *************************************************************************************************************************************************
+
+/* Exercise 2:
+
+Goal: Write a query to format the view_item event into a table with the appropriate columns
+(This replicates what we had in the slides, but it is missing a column).
+
+Code:*/
+
+SELECT event_id
+	,event_time
+	,user_id
+	,platform
 	,(
 		CASE 
 			WHEN paramater_name = item_id
@@ -39,15 +58,6 @@ GROUP BY event_id
 	,event_time
 	,user_id
 	,platform;
-  
--- *************************************************************************************************************************************************
-
-/* Exercise 2:
-
-Goal: Write a query to format the view_item event into a table with the appropriate columns
-(This replicates what we had in the slides, but it is missing a column).
-
-Code:*/
 
 -- *************************************************************************************************************************************************
 
@@ -56,5 +66,27 @@ Code:*/
 Goal: Use the result from the previous exercise, but make sure
 
 Code:*/
+
+SELECT event_id
+	,event_time
+	,user_id
+	,platform
+	,MAX(CASE 
+			WHEN parameter_name = 'item_id'
+				THEN CAST(parameter_value AS INT)
+			ELSE NULL
+			END) AS item_id
+	,MAX(CASE 
+			WHEN parameter_name = 'referrer'
+				THEN parameter_value
+			ELSE NULL
+			END) AS referrer
+FROM dsv1069.events
+WHERE event_name = 'view_item'
+GROUP BY event_id
+	,event_time
+	,user_id
+	,platform
+ORDER BY event_id;
 
 -- *************************************************************************************************************************************************
