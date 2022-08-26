4d6c7c78a1487c7fe64799dcd90fa2300eb23942
Marcos Henrique <marcoshsq@gmail.com>
2022-08-26 08:46:04 -0300

    Update 03. Product Analysis.sql

--- Course 02 - Data Wrangling Analysis AB Testing/Week_03/03. Product Analysis.sql
+++ Course 02 - Data Wrangling Analysis AB Testing/Week_03/03. Product Analysis.sql
@@ -18,11 +18,17 @@ that contains the course data.
 
 -- Code:
 
+SELECT COUNT(ID) AS id_count
+FROM dsv1069.users;
+
 -- ********************************************************************************************************************
 
 -- Exercise 1: Find out how many users have ever ordered
 
 -- Code:
+
+SELECT COUNT(DISTINCT user_id) AS count_users_ordered
+FROM dsv1069.orders;
 
 -- ********************************************************************************************************************
 
@@ -30,11 +36,31 @@ that contains the course data.
 
 -- Code:
 
+SELECT COUNT(DISTINCT user_id) AS users_who_reordered
+FROM (
+	SELECT user_id
+		,item_id
+		,COUNT(DISTINCT line_item_id) AS times_user_ordered
+	FROM dsv1069.orders
+	GROUP BY user_id
+		,item_id
+	) user_level_orders
+WHERE times_user_ordered > 1;
+
 -- ********************************************************************************************************************
 
 -- Exercise 3: Do users even order more than once?
 
 -- Code:
+
+SELECT COUNT(DISTINCT user_id)
+FROM (
+	SELECT user_id
+		,COUNT(DISTINCT invoice_id) AS order_count
+	FROM dsv1069.orders
+	GROUP BY user_id
+	) user_level
+WHERE order_count > 1;
 
 -- ********************************************************************************************************************
 
@@ -42,11 +68,23 @@ that contains the course data.
 
 -- Code:
 
+SELECT item_id
+	,COUNT(line_item_id) AS times_ordered
+FROM dsv1069.orders
+GROUP BY item_id
+ORDER BY times_ordered DESC;
+
 -- ********************************************************************************************************************
 
 -- Exercise 5: Orders per category
 
 -- Code:
+
+SELECT item_category
+	,COUNT(line_item_id) AS times_ordered
+FROM dsv1069.orders
+GROUP BY item_category
+ORDER BY times_ordered DESC;
 
 -- ********************************************************************************************************************
 
@@ -54,8 +92,45 @@ that contains the course data.
 
 -- Code:
 
+SELECT item_category
+	,AVG(times_category_ordered) AS avg_times_category_ordered
+FROM (
+	SELECT user_id
+		,item_category
+		,COUNT(DISTINCT line_item_id) AS times_category_ordered
+	FROM dsv1069.orders
+	GROUP BY user_id
+		,item_category
+	) user_level
+GROUP BY item_category;
+
 -- ********************************************************************************************************************
 
 -- Exercise 7: Goal: Find the average time between orders and Decide if this analysis is necessary!
 
 -- Code:
+
+SELECT first_orders.user_id
+	,DATE (first_orders.paid_at) AS first_order_date
+	,DATE (second_orders.paid_at) AS second_order_date
+	,DATE (second_orders.paid_at) - DATE (first_orders.paid_at) AS date_diff
+FROM (
+	SELECT user_id
+		,invoice_id
+		,paid_at
+		,DENSE_RANK() OVER (
+			PARTITION BY user_id ORDER BY paid_at ASC
+			) AS order_num
+	FROM dsv1069.orders
+	) first_orders
+JOIN (
+	SELECT user_id
+		,invoice_id
+		,paid_at
+		,DENSE_RANK() OVER (
+			PARTITION BY user_id ORDER BY paid_at ASC
+			) AS order_num
+	FROM dsv1069.orders
+	) second_orders ON first_orders.user_id = second_orders.user_id
+WHERE first_orders.order_num = 1
+	AND second_orders.order_num = 2;
