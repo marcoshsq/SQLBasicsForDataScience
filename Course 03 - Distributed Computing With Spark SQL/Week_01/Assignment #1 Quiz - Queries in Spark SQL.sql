1713cc629d5c9107161ba58e294e4541fc4b5487
Marcos Henrique <marcoshsq@gmail.com>
2022-09-04 20:11:37 -0300

    Update Assignment #1 Quiz - Queries in Spark SQL.sql

--- Course 03 - Distributed Computing With Spark SQL/Week_01/Assignment #1 Quiz - Queries in Spark SQL.sql
+++ Course 03 - Distributed Computing With Spark SQL/Week_01/Assignment #1 Quiz - Queries in Spark SQL.sql
@@ -1,15 +1,15 @@
-1. What is the first value for "Incident Number"?
+-- 1. What is the first value for "Incident Number"?
 
-2. What is the first value for "Incident Number" on April 4th, 2016?
+-- 2. What is the first value for "Incident Number" on April 4th, 2016?
 
-3. Is the first fire call in this table on Brooke or Conor's birthday?  Conor's birthday is 4/4 and Brooke's is 9/27 (in MM/DD format).
+-- 3. Is the first fire call in this table on Brooke or Conor's birthday?  Conor's birthday is 4/4 and Brooke's is 9/27 (in MM/DD format).
 
-4. What is the "Station Area" for the first fire call in this table?  Note that this table is a subset of the dataset.
+-- 4. What is the "Station Area" for the first fire call in this table?  Note that this table is a subset of the dataset.
 
-5. How many incidents were on Conor's birthday in 2016?
+-- 5. How many incidents were on Conor's birthday in 2016?
 
-6. How many fire calls had an "Ignition Cause" of "4 act of nature"?
+-- 6. How many fire calls had an "Ignition Cause" of "4 act of nature"?
+ 
+-- 7. What is the most common "Ignition Cause"? 
 
-7. What is the most common "Ignition Cause"? 
-
-8. What is the total incidents from the two joined tables?
+-- 8. What is the total incidents from the two joined tables?
