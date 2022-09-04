08283af8fc7349c9df4c79cd666c97446081eeaa
Marcos Henrique <marcoshsq@gmail.com>
2022-09-04 20:49:57 -0300

    Update Assignment #1 Quiz - Queries in Spark SQL.sql

--- Course 03 - Distributed Computing With Spark SQL/Week_01/Assignment #1 Quiz - Queries in Spark SQL.sql
+++ Course 03 - Distributed Computing With Spark SQL/Week_01/Assignment #1 Quiz - Queries in Spark SQL.sql
@@ -1,15 +1,23 @@
--- 1. What is the first value for "Incident Number"?
+1. What is the first value for "Incident Number"?
+Answer: 16000003
 
--- 2. What is the first value for "Incident Number" on April 4th, 2016?
+2. What is the first value for "Incident Number" on April 4th, 2016?
+Answer: 16037478
 
--- 3. Is the first fire call in this table on Brooke or Conor's birthday?  Conor's birthday is 4/4 and Brooke's is 9/27 (in MM/DD format).
+3. Is the first fire call in this table on Brooke or Conor's birthday?  Conor's birthday is 4/4 and Brooke's is 9/27 (in MM/DD format).
+Answer: Conor
 
--- 4. What is the "Station Area" for the first fire call in this table?  Note that this table is a subset of the dataset.
+4. What is the "Station Area" for the first fire call in this table?  Note that this table is a subset of the dataset.
+Answer: 29
 
--- 5. How many incidents were on Conor's birthday in 2016?
+5. How many incidents were on Conor's birthday in 2016?
+Answer: 80
 
--- 6. How many fire calls had an "Ignition Cause" of "4 act of nature"?
+6. How many fire calls had an "Ignition Cause" of "4 act of nature"?
+Answer: 5
  
--- 7. What is the most common "Ignition Cause"? 
+7. What is the most common "Ignition Cause"? 
+Answer: 2 unintentional
 
--- 8. What is the total incidents from the two joined tables?
+8. What is the total incidents from the two joined tables?
+Answer: 847094402
