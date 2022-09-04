9739432f626782432955b449f255097fa62df1fc
Marcos Henrique <marcoshsq@gmail.com>
2022-09-04 20:47:58 -0300

    Update and rename My_notes.sql to Assignment #2 Quiz - Spark Internals.sql

---
+++ Course 03 - Distributed Computing With Spark SQL/Week_02/Assignment #2 Quiz - Spark Internals.sql
@@ -0,0 +1,14 @@
+1. How many fire calls are in our table?
+Answer: 240613
+
+2. How large is our fireCalls dataset in memory? Input just the numeric value (e.g. 51.2)
+Answer: 59.6 MiB
+
+3. Which Unit Type is most common?
+Answer: ENGINE
+
+4. What type of transformation, wide or narrow, did the GROUP BY and ORDER BY queries result in?
+Answer: Wide
+
+5. Looking at the query below, how many tasks are in the last stage of the last job?
+Answer: 1
