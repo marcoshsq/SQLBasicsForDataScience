481b7acc82e8ceea7a1758eb39f25efdd147af09
Marcos Henrique <marcoshsq@gmail.com>
2022-09-09 01:33:26 -0300

    Update and rename My_notes.sql to Assignment #3 Quiz - Engineering Data Pipelines.sql

---
+++ Course 03 - Distributed Computing With Spark SQL/Week_03/Assignment #3 Quiz - Engineering Data Pipelines.sql
@@ -0,0 +1,14 @@
+1. What type of table is "newTable"?
+Answer: EXTERNAL
+
+2. How many rows are in "newTable"?
+Answer: 191039
+
+3. What is the "Battalion" of the first entry in the sorted table?
+Answer: B01
+
+4. Was this query faster or slower on the table with increased partitions?
+Answer: Slower
+
+5. Does the data stored within the table still exist at the original location ('dbfs:/tmp/newTableLoc') after you dropped the table?
+Answer: YES
