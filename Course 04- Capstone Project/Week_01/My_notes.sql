7bd3f9775c7c07e5439a84fc96b971b3717c372e
Marcos Henrique <marcoshsq@gmail.com>
2022-10-20 17:55:29 -0300

    Create My_notes.sql

---
+++ Course 04- Capstone Project/Week_01/My_notes.sql
@@ -0,0 +1,30 @@
+You are a data scientist working for a data analytics firm.  
+Your firm has explored a multitude of data sources and is tasked with providing key insights that your clients can make actionable. 
+Your manager has asked you to provide some data analytics guidance for one of the firm’s clients.
+
+In a typical scenario, you would iteratively work with your client to understand the data wanting to be analyzed.  
+Having a solid understanding of the data and any underlying assumptions present is crucial to the success of a data analysis project.  
+However, in this case, you will need to do a little more of the “heavy lifting”.
+
+To begin, you will prepare a project proposal detailing: 
+
+- The questions we are wanting to answer, 
+- initial hypothesis about the data relationships, and 
+- the approach you will take to get your answers.  
+
+NOTE: The proposal is just a plan for how we will travel.  It’s there to help keep you on your path by keeping the end goal in mind.  You will then will execute your plan and in the end present your findings in a month to your management.
+
+Review criteria: 
+
+The project proposal you will develop will guide you where you want to go, 
+but may change along the way; and that’s OK!  To kick things off you will need to:
+
+- Select your client;
+- Import your dataset;
+- Explore and understand your data; 
+- Develop an Entity Relationship Diagram (ERD).
+
+For this milestone, you will upload a PDF version of the two key steps needed in developing your project proposal:
+
+- Preparing for Your Project Proposal;
+- Develop Your Project Proposal.
