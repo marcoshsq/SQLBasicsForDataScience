43867a9670c6df45b777698916d1768ea67990db
Marcos Henrique <marcoshsq@gmail.com>
2022-10-20 18:12:32 -0300

    Update My_notes.sql

--- Course 04- Capstone Project/Week_04/My_notes.sql
+++ Course 04- Capstone Project/Week_04/My_notes.sql
@@ -1,16 +1,59 @@
-1. H​ow many calls of 'Call_Type_Group' "Fire"?
+Now that you’ve completed your analysis, it’s time to tell the story.  
+In this milestone, we will look at different ways we can present your findings.  
+Many presentations to management are via presentation software such as MS Powerpoint / Apple KeyNote / Google Slides and are typically static: 
+bullet points and clipped charts. 
 
-- 4196
+Sometimes, however, you may want a more dynamic approach to tell the story.  
+It all boils down to understanding your audience and the goal of your presentation.  
 
-2. H​ow many rows are in 'fireCallsGroupCleaned'?
+“Who?” 
+Who are you presenting to?
 
-- 134198
+- Colleague? Team?
+- Your Manager/Supervisor?
+- Sr. Management?
+- Customer?
+- External audience?
 
-3. W​hat is the accuracy of our model on test data as a percentage? Round to the nearest percent. (e.g. an accuracy of ".125" should be reported as "13")
+“Why?”
+Why do you want to present to this audience?
 
-- 82
+- Sharing process and findings?
+- Status / Plans?
+- Seeking additional funding? Strategy Setting?
+- Insights for customers?
+- Education or case studies?
 
-4. W​hat two values are in the 'prediction' column?
+“What?”
+What should you share?
 
-- 0
-- 1
+- What level of detail?
+- More text / More visualization?
+- Key points or nitty-gritty code snippets?
+- Bottom line info or high level trends?
+- Sensitive Data or publicly consumable?
+
+"How?"
+It all depends on the above … 
+
+Typically, the higher up the “ladder” you present, the less detail you are providing; 
+this doesn’t mean that you won’t be asked detailed questions (that you’ll need to have answers for),
+but you typically will have less time to communicate.
+
+For Status/Plans, Strategy, Funding, Education/Case Studies, these are typically good places to use presentation 
+software to gather your results and place your data in a position to tell the story.
+
+Start at the 10,000 foot level view and work your way down to what’s closest to the audience and end with your “ask” or your “key points”
+
+Don’t fill your slides with text, just bullets to guide you
+
+“A picture is worth a thousand words” … if you can create a graphic to tell the story (charts, infographics) then do it!
+
+When sharing the process and findings or working directly with customers on insights -- these are good opportunities to use more dynamic techniques
+
+Software like Jupyter notebooks combined with markdown, code, charts, and results allow you to be flexible 
+and respond to what-if questions more easily. You’ll be in your working environment and can show not only 
+what you did to get to your results, but the results themselves. If you need to try something, you can.
+
+Software like Tableau allows you to build visualizations and storyboards connecting to differing data sources 
+locally or remotely. This is great for quick exploratory analysis and putting together findings quickly as well.
