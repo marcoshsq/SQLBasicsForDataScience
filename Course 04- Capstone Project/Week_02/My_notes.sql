9b6bf1a07aa06698411f237afe09ba9b425ab2e6
Marcos Henrique <marcoshsq@gmail.com>
2022-10-20 17:52:45 -0300

    Update My_notes.sql

--- Course 04- Capstone Project/Week_02/My_notes.sql
+++ Course 04- Capstone Project/Week_02/My_notes.sql
@@ -1,15 +1,41 @@
-1. H​ow many fire calls are in our table?
+We’ve done the prep, now it’s time to dive in and start executing our Project Proposal.
+You’ve done the hard part; making the plan.  
+Now it’s time to get to the fun part!  
+Start looking at your data and perform some initial stats to get a good feel about what you have available to you.
 
-- 240613
+This will build on your cursory exploration in Module 1 and Module 2. 
+You may have already gone deeper, if so, that’s OK; you’re ahead of the game! 
+In this module you’ll focus your efforts on understanding the ins-and-outs of the data and perform some descriptive stats.  
+This will give you a solid grounding in your dataset and be able to EXPLAIN the data and your findings.
 
-2. W​hich "Unit Type" is the most common?
+Clients often (even though they know their data) will ask questions about what you present, 
+like “How did you come to this conclusion? Does our data cover x, y, and z? I didn’t know that ‘a’ was connected to ‘b’!”
+You will face these types of questions and more, so having a solid foundation in what you are analyzing is a must. 
 
-- ENGINE
+Keep your questions and your hypotheses in mind; 
+it’s very easy to start down “rabbit-holes” and have to bring yourself back up for air to realize that you lost time chasing that rabbit.  
+Nonetheless, it’s part of the journey
 
-3. W​hat type of transformation, wide or narrow, did the 'GROUP BY' and 'ORDER BY' queries result in?
+What if your hypothesis is wrong? No problem!
 
-- Wide
+You've discovered a relationship that was thought to be present but isn't! 
+That's great analysis! So now what?
 
-4. H​ow many tasks were in the last stage of the last job?
+Yes, sometimes we may go down a path that is a dead-end, and that’s OK.  
+If all your paths are barren, pick a new path, but don’t stop; 
+keep moving forward!
 
-- 2
+Document your discovery … this may be something your client didn’t know or was an assumption they made as well.
+
+Insights are insights and data is data … you are not responsible for what the data says, it just is. 
+Your job is to let the data speak to you.
+
+What if your hypothesis is right? Yay for you
+
+You discovered something out of your initial analysis of the data, relationships and assumptions that proved to be true. Now what?
+
+Start thinking about how to characterize that finding? What’s the story you want to tell?
+
+How best to tell it? You can think about visually, just key points, a narrative, or use the raw data?
+
+Iterate on these steps to get a few good hypothesis/questions to answer and prove out.
